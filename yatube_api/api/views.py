daf71cc274927a7bd3fd67eed14978f91e0a03fd
Kirill Lisitsynskiy <kirill.lisitsynskiy@gmail.com>
2022-01-14 22:37:03 +0800

    Second version: delete perform_create() from FollowViewSet

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -46,6 +46,3 @@ class FollowViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
 
     def get_queryset(self):
         return self.request.user.follower.all()
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
